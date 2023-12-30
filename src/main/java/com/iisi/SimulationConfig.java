777f8a16badb2bcda16ce29c4a5a7ff77ff486d2
oxygensend <szymon19314@wp.pl>
2023-12-30 11:43:03 +0100

    add probability of incident

--- src/main/java/com/iisi/SimulationConfig.java
+++ src/main/java/com/iisi/SimulationConfig.java
@@ -21,10 +21,10 @@ public class SimulationConfig {
     public static final int MIN_FIRING_STRENGTH = 20;
     public static final int MAX_FIRING_STRENGTH = 90;
 
-    public static final Map<District.ThreatLevel, Integer> MAX_INCIDENTS_BY_THREAT_LEVEL_PER_1000_ITERATIONS = Map.of(
-            District.ThreatLevel.LOW, 2,
-            District.ThreatLevel.MEDIUM, 5,
-            District.ThreatLevel.HIGH, 10
+    public static final Map<District.ThreatLevel, Double> PROBABILITY_OF_INCIDENT_BY_THREAT_LEVEL = Map.of(
+            District.ThreatLevel.LOW, 0.05,
+            District.ThreatLevel.MEDIUM, 0.15,
+            District.ThreatLevel.HIGH, 0.3
     );
 
     public static final Map<District.Districts, List<Point>> DISTRICT_BOUNDARIES_CONFIG = Map.of(
